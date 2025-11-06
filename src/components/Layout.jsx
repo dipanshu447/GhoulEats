32b3e519e48577b836bc24965e1f96009f04d71d
dipanshu447 <dipanshusahu447@gmail.com>
2025-11-06 19:17:48 +0530

    add: added navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,10 +4,10 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout(){
     return (
-        <>
+        <div className="box-border w-screen h-screen bg-[#1b0917] text-white">
             <Navbar />
             <Outlet />
             <Footer />
-        </>
+        </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,20 @@
-export default function Navbar(){
-    return <h1>Navbar</h1>
+import logo from '../assets/Logo.png'
+import { Link, NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+    const activeRoutestyle = ({isActive}) => isActive ? 'px-4 py-1.5 rounded-full font-bold transition-all duration-500 ease-in-out cursor-pointer text-[#EB5B00]' : 'px-4 py-1.5 rounded-full hover:font-bold transition-all duration-500 ease-in-out cursor-pointer hover:text-[#EB5B00]';
+    return (
+        <div className="px-20 py-6 flex justify-between items-center transition-all duration-500 ease-in-out ">
+            <Link to='/' className='w-38'>
+                <img src={logo} alt="logo" className='object-cover' />
+            </Link>
+            <div className='flex gap-4'>
+                <NavLink to='/menu' className={activeRoutestyle}>Menu</NavLink>
+                <NavLink to='/about' className={activeRoutestyle}>About</NavLink>
+                <NavLink to='/events' className={activeRoutestyle}>Events</NavLink>
+                <NavLink to='/join' className={activeRoutestyle}>Join Us</NavLink>
+            </div>
+            <button className='px-3.5 py-1 rounded-full cursor-pointer border border-[#EB5B00] hover:bg-[#EB5B00] hover:text-black hover:font-medium transition-all duration-500 ease-in-out'>Sign Up</button>
+        </div>
+    )
 }
