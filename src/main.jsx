44e17603cdd3929123114ce05998dd08d96086b1
dipanshu447 <dipanshusahu447@gmail.com>
2025-11-06 10:20:43 +0530

    chore: configured all page routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout';
+import Welcome from './pages/Welcome';
+import AboutUs from './pages/AboutUs';
+import ErrorPage from './pages/ErrorPage';
+import JoinPage from './pages/JoinPage';
+import Menu from './pages/Menu';
+import Notfound from './pages/Notfound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Welcome />
+      },
+      {
+        path: '/menu',
+        element: <Menu />
+      },
+      {
+        path: '/about',
+        element: <AboutUs />
+      },
+      {
+        path: '/join',
+        element: <JoinPage />
+      },
+      { path: '*', element: <Notfound /> }
+    ],
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/JoinPage.jsx
@@ -0,0 +1,3 @@
+export default function JoinPage(){
+    return <h1>JoinPage Page</h1>
+}
