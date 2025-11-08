31e071af1c8b457bf70a702e2bd7a025dc1c3cf7
dipanshu447 <dipanshusahu447@gmail.com>
2025-11-08 21:44:30 +0530

    add: added not found page

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,3 +1,20 @@
-export default function NotFound(){
-    return <h1>NotFound Page</h1>
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+    return (
+        <div className="bg-[url('./assets/about_page/bg3.jpg')] bg-center text-center flex flex-col gap-8 mx-auto z-1 py-30 px-60 relative">
+            <div className="absolute inset-0 bg-linear-to-t from-transparent to-[#03031e] z-1"></div>
+            <div className="absolute inset-0 bg-linear-to-b from-transparent to-[#03031e] z-1"></div>
+            <div className="flex flex-col gap-2 font-[Eater] relative z-1">
+                <h1 className="text-6xl leading-tight">404 Page not <span className="text-[#EB5B00]">found</span></h1>
+                <h2 className="font-light text-[#EB5B00]">Looks like a hungry ghoul devoured this page!</h2>
+            </div>
+            <div className="mx-6 relative z-1">
+                Don’t worry, you can still make it back safely… before midnight.
+            </div>
+            <div className="flex gap-5 self-center relative z-1">
+                <Link to='/' className="py-1.5 px-4 border border-transparent bg-[#EB5B00] text-black hover:text-[#EB5B00] hover:bg-transparent hover:border-[#EB5B00] rounded-4xl cursor-pointer transition-all duration-500 ease-in-out">Return to the feast</Link>
+            </div>
+        </div>
+    )
 }
